c571da544f9922bc264c70ca3592bdd90f19565c
Mykkolla <tarabukin.n.10@gmail.com>
2023-05-04 22:07:00 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { FeedBackApp } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FeedBackApp />
   </React.StrictMode>
 );
